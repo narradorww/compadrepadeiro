52d4371abf8c6392a5ce6ee3af350a08cc661e0a
narradorww <rodrigo.anst@gmail.com>
2021-03-22 00:51:00 -0300

    Versao com calculo definido. falta estilizar

--- js/calculadoraDePao.js
+++ js/calculadoraDePao.js
@@ -1,9 +1,21 @@
-function qtdDeMassa(tamDoPao, quantosPaes) {
-    return tamDoPao * quantosPaes;
-}
-var farinha=qtdDeMassa*0.56;
-var agua=qtdDeMassa*0.28;
-var levain=qtdDeMassa*0.14;
-var sal= qtdDeMassa*0.02;
-console.log(`Você precisará de: ${farinha}de farinha, ${agua}de água, ${levain} de levain ${sal} de sal`)
+
+    function somar(){
+        var tamDoPao=window.document.getElementById('tamDoPao');
+         var quantosPaes=window.document.getElementById('quantosPaes');
+         var n1=parseInt(tamDoPao.value);
+         var n2=parseInt(quantosPaes.value);
+        var res=window.document.getElementById('res');
+        var totalDeMassa=n1*n2;
+        var farinha=totalDeMassa*0.58;
+        var agua=totalDeMassa*0.28;
+        var levain=totalDeMassa*0.14;
+        var sal=totalDeMassa*0.02;
+
+        res.innerHTML =`O total de massa da receita será <strong>${totalDeMassa}</strong>`;
+        far.innerHTML =`O total de farinha da receita será  <strong>${farinha.toFixed(0)}</strong>`;
+        water.innerHTML =`O total de água da receita será  <strong>${agua.toFixed(0)}</strong>`;
+        lev.innerHTML =`O total de levain da receita será <strong> ${levain.toFixed(0)}</strong>`;
+        salt.innerHTML =`O total de sal da receita será  <strong>${sal.toFixed(0)}</strong>`;
+        }
+
 
